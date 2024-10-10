be01cadc5a1b0133645aab6c421595a9429ac7e5
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-10-10 15:12:27 +0200

    Update

--- middlewares/Authentification.js
+++ middlewares/Authentification.js
@@ -2,15 +2,16 @@ import jsonwebtoken from "jsonwebtoken";
 import dotenv from "dotenv/config";
 
 export function authentification(request, response, next) {
-	if (request.headers.authorization == "" || request.headers.authorization == null) {
+	const token = request.headers.authorization;
+	const secret = process.env.SECRET;
+
+	if (token == "" || token == null) {
 		return response.status(401).json({
             message: "Something went wrong while checking authorizations !",
             error: 1,
             error_message: "A token is needed in headers>Authorization !",
         });
 	}
-    
-	const secret = process.env.SECRET;
 	
 	jsonwebtoken.verify(token, secret, (error, user) => {
         if (error) return response.status(401).json({
