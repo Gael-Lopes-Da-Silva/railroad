0bc699bc8950fe187dde0d13074143f46b8527d7
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-10-10 15:20:16 +0200

    Update

--- middlewares/CheckEmployee.js
+++ middlewares/CheckEmployee.js
@@ -1,16 +1,22 @@
-import UserModel from "../models/UserModel.js";
+import { getUser } from "../models/UserController.js";
 
 export function checkEmployee(request, response, next) {
-    const user = UserModel.findById(request.user.id);
-
-	if (user.role == "employee" || user.role == "admin") {
-		next();
-        return;
-	}
-
-    return response.status(403).json({
-        message: "Something went wrong while checking permissions !",
-        error: 1,
-        error_message: "You need to be an employee to do this action !",
+    getUser(request).then((user) => {
+        if (user.role == "employee" || user.role == "admin") {
+            next();
+            return;
+        }
+    
+        return response.status(403).json({
+            message: "Something went wrong while checking permissions !",
+            error: 1,
+            error_message: "You need to be an employee to do this action !",
+        });
+    }).catch((error) => {
+        return response.status(403).json({
+            message: "Something went wrong while checking permissions !",
+            error: 1,
+            error_message: error,
+        });
     });
 }
