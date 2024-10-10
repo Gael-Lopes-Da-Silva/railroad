bd0f02391abecf3f56c3d92cd49285fadc97a7e6
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-10-10 09:44:13 +0200

    Structure du projet

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const User = new mongoose.model("User", new mongoose.Schema({
-	username: { type: String, required: true, unique: true },
+	pseudo: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
-	permission: { type: String, require: true, default: "user" },
+	role: { type: String, require: true, default: "user" },
 	deletedAt: { type: Date, default: null },
 }, { timestamps: true }));
 
