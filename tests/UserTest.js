46a113e8feda8d3c295a9a2a11be174c444e8fd5
Jean <lauth_jean@live.fr>
2024-10-11 16:21:59 +0200

    Test chain register/login

--- tests/UserTest.js
+++ tests/UserTest.js
@@ -1,16 +1,19 @@
-import * as chai from "chai";
+import chai from "chai";
 import chaiHttp from "chai-http";
 import app from "../server.js";
+import User from "../routes/UserRouter.js";
 
 chai.use(chaiHttp);
 const expect = chai.expect;
 
 let userCreds;
+let userToken;
+let normalUserId;
 
 // create / log / get / getid / update / updateid / delete / deleteid
 before(() => {
   userCreds = {
-    pseudo: "TestUserTT",
+    pseudo: "TestUser1",
     email: "testuser@example.com",
     password: "Password123",
   };
@@ -22,9 +25,41 @@ describe("User registration and login", () => {
       .request(app)
       .post("/users/register")
       .send(userCreds);
-      
+
     expect(response).to.have.status(201);
-    expect(response.body).to.have.property("pseudo");
+    expect(response.body).to.have.property(
+      "message",
+      "User registered successfully !"
+    );
+    expect(response.body).to.have.property("error", 0);
+  });
+
+  it("Should login an user and set the token linked to", async () => {
+    const response = await chai
+      .request(app)
+      .post("/users/login")
+      .send(userCreds);
+
+    expect(response).to.have.status(200);
+    expect(response.body).to.have.property(
+      "message",
+      "User logged successfully !"
+    );
+    expect(response.body).to.have.property("error", 0);
+
+    userToken = response.body.accessToken;
+    normalUserId = response.body._id;
   });
 });
 
+describe("User entity tests", () => {
+  it("Should get the user", async () => {
+    const response = await chai
+      .request(app)
+      .get(`users/get/${normalUserId}`)
+      .set("authorization", `Bearer ${userToken}`);
+
+    expect(response).to.have.status(200);
+    expect(response.body).to.have.property("pseudo", userCreds.pseudo);
+  });
+});
