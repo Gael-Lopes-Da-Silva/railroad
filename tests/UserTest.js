b03f53b23acc37bc5a41b2caef82a8bf5cfeacd8
Jean <lauth_jean@live.fr>
2024-10-14 08:52:31 +0200

    Test User

--- tests/UserTest.js
+++ tests/UserTest.js
@@ -10,12 +10,22 @@ let userCreds;
 let userToken;
 let normalUserId;
 
+let adminCreds;
+let adminToken;
+let adminUserId;
+
 // create / log / get / getid / update / updateid / delete / deleteid
-before(() => {
+before(async () => {
   userCreds = {
     pseudo: "TestUser1",
     email: "testuser@example.com",
     password: "Password123",
+  };
+
+  adminCreds = {
+    pseudo: "AdminUser1",
+    email: "adminuser@example.com",
+    password: "AdminPassword123",
   };
 });
 
@@ -27,10 +37,6 @@ describe("User registration and login", () => {
       .send(userCreds);
 
     expect(response).to.have.status(201);
-    expect(response.body).to.have.property(
-      "message",
-      "User registered successfully !" // checker l'erreur et pas le message directement
-    );
     expect(response.body).to.have.property("error", 0);
   });
 
@@ -41,14 +47,53 @@ describe("User registration and login", () => {
       .send(userCreds);
 
     expect(response).to.have.status(200);
-    expect(response.body).to.have.property(
-      "message",
-      "User logged successfully !"
-    );
     expect(response.body).to.have.property("error", 0);
+    expect(response.body).to.have.property("token").that.is.a("string"); // check is the token is here and a string
 
     userToken = response.body.accessToken;
     normalUserId = response.body._id;
+  });
+});
+
+describe("Admin registration and login", () => {
+  it("Should register a second user (future admin)", async () => {
+    const response = await chai
+      .request(app)
+      .post("/users/register")
+      .send(adminCreds);
+
+    expect(response).to.have.status(201);
+    expect(response.body).to.have.property("error", 0);
+
+    adminUserId = response.body._id;
+  });
+
+  it("Should promote an user to admin directly in the database", async () => {
+    // Create a temp admin in the DB
+    await User.updateUser ({ _id: adminUserId }, { role: "admin" });
+  });
+
+  it("Shoud log in as the promoted admin user", async () => {
+    const response = await chai
+      .request(app)
+      .post("/users/login")
+      .send(adminCreds);
+
+    expect(response).to.have.status(200);
+    expect(response.body).to.have.property("token");
+
+    adminToken = response.body.token;
+  });
+
+  it("Should update another user's role to admin", async () => {
+    const response = await chai
+      .request(app)
+      .post(`/users/set/admin/${adminUserId}`)
+      .set("Authorization", `Bearer ${adminToken}`)
+      .send({ role: "admin" });
+
+    expect(response).to.have.status(200);
+    expect(response.body).to.have.property("error", 0);
   });
 });
 
